eef7e9c9dfd93c7d23e5d6f2eaa6fc9bb37fe16c
Sathya <sathya.andromeda@gmail.com>
2024-09-06 11:55:18 +0530

    Initial Commit

---
+++ Ironman/src/main/java/com/marvel/Ironman/IronmanApplication.java
@@ -0,0 +1,13 @@
+package com.marvel.Ironman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronmanApplication.class, args);
+	}
+
+}
---
+++ Ironman/src/test/java/com/marvel/Ironman/IronmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvel.Ironman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
